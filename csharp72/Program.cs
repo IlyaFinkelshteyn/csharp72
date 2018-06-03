cf68fa9d21aefab39ff36ce9ec21d463c59ee37d
ilyaf f <ilyaf@appveyor.com>
2018-06-02 22:45:45 -0700

    Initial commit

---
+++ csharp72/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp72
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await getHeaders(args[0]);
+        }
+
+        static private async Task getHeaders(string url)
+        {
+            WebResponse response = await WebRequest
+                .Create(url)
+                .GetResponseAsync()
+                .ConfigureAwait(false);
+            Console.WriteLine(response.Headers.ToString());
+        }
+    }
+}
